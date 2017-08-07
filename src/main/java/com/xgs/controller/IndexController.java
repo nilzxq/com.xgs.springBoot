0a36db2b183f2de49576c53960f2d047dae1f12a
nilzxq <2013358717@qq.com>
2017-08-07 18:51:20 +0800

    RestController

--- src/main/java/com/xgs/controller/IndexController.java
+++ src/main/java/com/xgs/controller/IndexController.java
@@ -1,17 +1,39 @@
 package com.xgs.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RestController 标志该类的所有请求方法 返回json格式 不做视图解析。
+ * @author zxq
+ *
+ */
 @RequestMapping("/springBoot")
-@Controller
+@RestController
 public class IndexController {
 
-	@ResponseBody
 	@RequestMapping("/index")
 	public String index() {
 		return "success";
 	}
+	@RequestMapping("/index1")
+	public String index1() {
+		return "success1";
+	}
 	
+	/**
+	 * SpringBoot返回list格式
+	 * @return
+	 */
+	@RequestMapping("/listIndex")
+	public List<String> listIndex(){
+		List<String> list=new ArrayList<String>();
+		list.add("index1");
+		list.add("index2");
+		list.add("index3");
+		return list;
+	}
 }
