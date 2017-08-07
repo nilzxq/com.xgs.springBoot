6b131c340abfea302706ea2bb8804815843b93d0
nilzxq <2013358717@qq.com>
2017-08-07 16:20:58 +0800

    ComponentScan

--- src/main/java/com/xgs/server/App.java
+++ src/main/java/com/xgs/server/App.java
@@ -2,13 +2,14 @@ package com.xgs.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-import com.xgs.controller.IndexController;
 /**
  * SpringBoot		∆Ù∂Ø¿‡
  * @author zxq
  *
  */
+@ComponentScan(basePackages= {"com.xgs.controller"})
 @EnableAutoConfiguration
 public class App {
 	public static void main(String[] args) {
